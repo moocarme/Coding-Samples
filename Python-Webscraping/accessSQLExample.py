94a2f84d50e45ff93c85f2b10e23667d4265a759
Matthew Moocarme <moocarme@gmail.com>
2016-07-13 17:55:22 -0400

    Add files via upload

--- Python-Webscraping/accessSQLExample.py
+++ Python-Webscraping/accessSQLExample.py
@@ -9,19 +9,23 @@ import sqlite3
 conn = sqlite3.connect('chordProgdb2.sqlite')
 cur = conn.cursor()
 
-cur.execute('''
-SELECT chord.prog, COUNT(*)
-FROM Song AS master
-LEFT JOIN Genre AS subgenre_tbl ON subgenre_tbl.id = master.subgenre_id
-LEFT JOIN Genre ON genre.id = master.genre_id
-LEFT JOIN ChordProg AS chord ON chord.id = master.chordProg_id
-WHERE subgenre_tbl.name = 'Americana'
-GROUP BY chord.prog
-ORDER BY COUNT(*) DESC
-LIMIT 11
-;
-''')
-
-top10Americana= cur.fetchall()
-conn.commit
-print(top10Americana)
+def main():
+    cur.execute('''
+    SELECT chord.prog, COUNT(*)
+    FROM Song AS master
+    LEFT JOIN Genre AS subgenre_tbl ON subgenre_tbl.id = master.subgenre_id
+    LEFT JOIN Genre ON genre.id = master.genre_id
+    LEFT JOIN ChordProg AS chord ON chord.id = master.chordProg_id
+    WHERE subgenre_tbl.name = 'Americana'
+    GROUP BY chord.prog
+    ORDER BY COUNT(*) DESC
+    LIMIT 11
+    ;
+    ''')
+    
+    top10Americana= cur.fetchall()
+    conn.commit
+    print(top10Americana)
+    
+if __name__=="__main__":
+    main()
