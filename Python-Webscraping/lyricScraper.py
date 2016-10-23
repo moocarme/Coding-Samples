cc42269890158bc30a7ef965db3038027c2a1f6a
Matthew Moocarme <moocarme@gmail.com>
2016-10-22 20:23:03 -0400

    Update lyricScraper.py

--- Python-Webscraping/lyricScraper.py
+++ Python-Webscraping/lyricScraper.py
@@ -4,31 +4,42 @@ Created on Sat Jul  2 16:49:11 2016
 
 @author: matt-666
 """
-import urllib
+from urllib import urlopen
 from bs4 import BeautifulSoup, SoupStrainer
-import string
-import re
-import pickle
+from string import maketrans, punctuation
+from re import match
+from pickle import dump
 
 # = Helper Functions =========================================================
 def visible(element):
+    '''
+    Filters all extraneous parts of he webpage out, and returns true only for 
+    the visible text in the webpage
+    '''
     if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
         return False
-    elif re.match('<!--.*-->', str(element.encode('utf-8'))):
+    elif match('<!--.*-->', str(element.encode('utf-8'))):
         return False
     return True
 
 def strClean(strList):
+    '''
+    Cleans string by removing extraneous text on ends such as adverisements and
+    text on every webpage and returns just the lyrics for the song
+    '''
     newRes=strList[276:-57] # specific to this website
     newRes2 = []
     for i in range(len(newRes)-1):
         if newRes[i+1]!='\n' and newRes[i]!='\n': 
             t1 = ' '.join(str(newRes[i]).split()) # removes the \n, which may be needed
-            t1 = t1.translate(string.maketrans("",""), string.punctuation)
+            t1 = t1.translate(maketrans("",""), punctuation)
             newRes2.extend(str.split(t1.lower()))
     return newRes2
     
 def get_links(url, lookfor):
+    '''
+    Looks for just the links of a webpage that have a certain sub-string in them
+    '''
     list_of_links = []
     for link in BeautifulSoup(url, parse_only=SoupStrainer('a')):
         if link.has_attr('href') and lookfor in link['href']:
@@ -42,29 +53,34 @@ def get_links(url, lookfor):
 class lyrics_scraper(object):
     
     def __init__(self, url):
-        self.baseurl = urllib.urlopen(url).read()
+        self.baseurl = urlopen(url).read()
         
     def get_all_links(self):
+        '''
+        Gets all the song links on the website
+        '''
         # = Get all links by artist first letter
         artistAlphaLinks = get_links(self.baseurl, '.com/artist')    
          
         # = Get all links of artists 
         artistLinks = []
         for alphaLink in artistAlphaLinks:    
-            newurl = urllib.urlopen(alphaLink).read()
+            newurl = urlopen(alphaLink).read()
             artistLinks.extend(get_links(newurl, '.com/show/artist'))
         
         # = Get all links of songs  
         self.songLinks = []
         for artistLink in artistLinks:    
-            newArtisturl = urllib.urlopen(artistLink).read()
+            newArtisturl = urlopen(artistLink).read()
             self.songLinks.extend(get_links(newArtisturl, '.com/lyrics/'))
         
     def get_corpus(self):
-        # = Get the lyrics of all songs 
+        '''
+        Get the lyrics from all songslinks on the website
+        '''
         self.songLyrics = []
         for songLink in self.songLinks:
-            songSoup = BeautifulSoup(urllib.urlopen(songLink).read())    
+            songSoup = BeautifulSoup(urlopen(songLink).read())    
             songData = songSoup.findAll(text=True)
             songResult = filter(visible, songData)
             self.songLyrics.append(strClean(songResult)) ## Extend -> one long list, append -> list length of songs
@@ -78,4 +94,4 @@ if __name__ == "__main__":
     scraper.getLyrics()
     corpus = scraper.get_corpus
     # = dump in pickle file
-    pickle.dump(corpus, open("songLyrics.p","wb"))
+    dump(corpus, open("songLyrics.p","wb"))
